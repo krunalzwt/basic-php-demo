aac7bdbb81262be9599a64aa8a724129dc684ad8
krunalbalas <12102080601070@adit.ac.in>
2025-03-01 19:52:14 +0530

    mysql connection updated

--- common/db.php
+++ common/db.php
@@ -4,11 +4,11 @@ $username = "root";
 $password = "";
 $dbname = "discuss";
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password);
 if ($conn->connect_error) {
     echo "not connected!";
+    exit();
 }
-
 
 //DATABASE QUERY
 $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
@@ -21,11 +21,26 @@ $conn->select_db($dbname);
 
 
 //TABLE QUERIES
+$create_category_table = "CREATE TABLE IF NOT EXISTS category(
+    id INT(30) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+)";
+
 $create_users_table = "CREATE TABLE IF NOT EXISTS users (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     email VARCHAR(256) NOT NULL UNIQUE,
     password VARCHAR(256) NOT NULL
+)";
+
+$create_questions_table = "CREATE TABLE IF NOT EXISTS questions (
+    id INT(10) AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    category_id INT(10) NOT NULL,
+    user_id INT(10) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
 )";
 
 $create_answers_table = "CREATE TABLE IF NOT EXISTS answers (
@@ -37,32 +52,22 @@ $create_answers_table = "CREATE TABLE IF NOT EXISTS answers (
     FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
 )";
 
-$create_questions_table = "CREATE TABLE IF NOT EXISTS questions (
-    id INT(10) AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    description VARCHAR(300) NOT NULL,
-    category_id INT(10) NOT NULL,
-    user_id INT(10) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-)";
 
-$create_category_table = "CREATE TABLE IF NOT EXISTS category(
-    id INT(30) AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-)";
+// Execute table creation
+if ($conn->query($create_category_table) === FALSE) {
+    echo "Error creating category table: " . $conn->error . "<br>";
+}
 
 if ($conn->query($create_users_table) === FALSE) {
     echo "Error creating users table: " . $conn->error . "<br>";
 }
 
-if ($conn->query($create_answers_table) === FALSE) {
-    echo "Error creating users table: " . $conn->error . "<br>";
-} 
-
 if ($conn->query($create_questions_table) === FALSE) {
-    echo "Error creating users table: " . $conn->error . "<br>";
+    echo "Error creating questions table: " . $conn->error . "<br>";
 }
 
-if ($conn->query($create_category_table) === FALSE) {
-    echo "Error creating users table: " . $conn->error . "<br>";
+if ($conn->query($create_answers_table) === FALSE) {
+    echo "Error creating answers table: " . $conn->error . "<br>";
 }
+
+?>
